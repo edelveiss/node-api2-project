9f4da4818ea95cb869035594e638d9c8bff159e3
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-07-06 15:57:05 -0700

    MVP are finished

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const postsRouter = require("./posts/posts-router");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/posts", postsRouter);
+//API test
+server.get("/", (req, res) => {
+  res.send(`
+        <h2>Lambda Posts API</h>
+        <p>Welcome to the Lambda Posts API</p>
+      `);
+});
+
+server.listen(4000, () => {
+  console.log("\n*** Server Running on http://localhost:4000 ***\n");
+});
